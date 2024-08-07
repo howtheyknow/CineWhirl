7735e20756dc04316f88300445c7f83049e57bc2
fanlimgames <128323567+fanlimgames@users.noreply.github.com>
2024-08-07 14:37:50 +0200

    .

--- themes/all.ts
+++ themes/all.ts
@@ -3,13 +3,11 @@ import blue from "./list/blue";
 import red from "./list/red";
 import gray from "./list/gray";
 import blck from "./list/blck";
-import golden from "./list/golden";
 
 export const allThemes = [
   teal,
   blue,
   gray,
   red,
-  blck,
-  golden
+  blck
 ]
