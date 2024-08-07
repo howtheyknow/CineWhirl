3e5ff41776c7e6c481f9f2bb15a381dec341bf3e
fanlimgames <128323567+fanlimgames@users.noreply.github.com>
2024-08-07 14:15:03 +0200

    adding new theme

--- themes/all.ts
+++ themes/all.ts
@@ -3,11 +3,13 @@ import blue from "./list/blue";
 import red from "./list/red";
 import gray from "./list/gray";
 import blck from "./list/blck";
+import golden from "./list/golden";
 
 export const allThemes = [
   teal,
   blue,
   gray,
   red,
-  blck
+  blck,
+  golden
 ]
